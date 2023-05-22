8e783ee79c29b560380a97f702e7eb52482f5cd9
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-22 01:31:53 -0400

    Add nicer css

--- contentScript.js
+++ contentScript.js
@@ -9,28 +9,38 @@ const createPopupElement = (tag, styles, textContent) => {
 
   const message = "Happy browsing!"
 
+  const justMessageHeight = "80px";
+  const dismissButtonSpacingFromMainText = justMessageHeight;
   
   const popup = createPopupElement("div", {
     position: "fixed",
     top: "10px",
     right: "10px",
     padding: "10px",
+    borderRadius: "10px",
     backgroundColor: "rgba(0, 0, 0, 0.8)",
     color: "#fff",
     fontFamily: "Arial, sans-serif",
     fontSize: "16px",
+    textAlign: "right",
     zIndex: "9999",
+    boxShadow: "10px 5px 5px blue",
+    margin: "0em",
+    // width: "",
   }, message);
   
   const dismissButton = createPopupElement("button", {
     position: "absolute",
-    top: "10px",
+    top: dismissButtonSpacingFromMainText,
     right: "5px",
-    backgroundColor: "transparent",
-    border: "none",
+    padding: "10px",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
     color: "#fff",
     fontSize: "16px",
+    borderRadius: "10px",
     cursor: "pointer",
+    boxShadow: "10px 5px 5px blue",
+    width: "100px",
   }, "Dismiss");
   
 function removePopup() {
